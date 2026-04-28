5fd590afb9a9a15ea8fdec6a0da1389c43d00aed
salvadorpulido-commits <salvador.pulido@iesnervion.es>
2026-04-28 13:16:49 +0200

    ficherios ejercicio02

---
+++ src/ejercicios/ficheros/Ejercicio2.java
@@ -0,0 +1,37 @@
+package ejercicios.ficheros;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+  
+        File archivo = new File("Enteros.txt");
+        
+        int suma = 0;
+        int contador = 0;
+
+        try (Scanner sc = new Scanner(archivo)) {
+        	
+            // Leemos números enteros 
+        	
+            while (sc.hasNextInt()) {
+                int num = sc.nextInt();
+                suma += num;
+                contador++;
+            }
+
+            if (contador > 0) {
+                double media = (double) suma / contador; // Cast a double para no perder decimales 
+                System.out.println("Suma de enteros: " + suma);
+                System.out.println("Media aritmética: " + media);
+            } else {
+                System.out.println("El fichero Enteros.txt está vacío.");
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: No se encuentra 'Enteros.txt' en la raíz del proyecto.");
+        }
+    }
+}
